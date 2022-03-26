87cb40c17025c39a6f3419140de61d8c4540b7d5
Stake322 <81745125+Stake322@users.noreply.github.com>
2022-03-26 15:55:18 +0400

    and game components

--- src/components/game-item/game-item.js
+++ src/components/game-item/game-item.js
@@ -1,24 +1,25 @@
 import React from "react";
-
+import { GameCover } from "../game-cover/game-cover";
 import "./game-item.css";
+import { GameBuy } from "../game-buy/game-buy";
+import { GameGenre } from "../game-genre/game-genre";
 
 export const GameItem = ({ game }) => {
 
-
-
-  return (
-    <div className="game-item" >
-      <div className="game-item__details">
-        <span className="game-item__title">{game.title}</span>
-        <div className="game-item__genre">
-          {game.genres.map((genre) => (
-              genre
-          ))}
+    return (
+        <div className="game-item" >
+            <GameCover image={game.image} />
+            <div className="game-item__details">
+                <span className="game-item__title">{game.title}</span>
+                <div className="game-item__genre">
+                    {game.genres.map((genre) => (
+                        <GameGenre genre={genre} />
+                    ))}
+                </div>
+                <div className="game-item__buy">
+                    <GameBuy game={game} />
+                </div>
+            </div>
         </div>
-        <div className="game-item__buy">
-          Buy
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
